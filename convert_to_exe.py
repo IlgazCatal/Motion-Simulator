1c77fe791ff42d4d7b1bfd46bcd18013d1e41da2
IlgazCatal <97466646+IlgazCatal@users.noreply.github.com>
2023-05-21 18:44:11 +0300

    Update convert_to_exe.py

--- convert_to_exe.py
+++ convert_to_exe.py
@@ -6,6 +6,6 @@ sys.argv.append('py2exe')
 setup(
     py_modules=[],
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': r"C:\\Users\\ilgaz\Documents\\Fizik Projesi\\main.py"}],
+    windows = [{'script': r"PATH_TO_MAIN"}],
     zipfile = None,
 )
